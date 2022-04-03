33300eef8dfea9f747a25f10b19c6cf15b75f096
Ritesh Mishra <riteshmishra15011992@gmail.com>
2022-04-03 11:40:49 +0530

    helloworld

---
+++ hello_world_module/hello_world.c
@@ -0,0 +1,38 @@
+/***************************************************************************//**
+*  \file       driver.c
+*
+*  \details    Simple hello world driver
+*
+*  \author     EmbeTronicX
+*
+* *******************************************************************************/
+#include<linux/kernel.h>
+#include<linux/init.h>
+#include<linux/module.h>
+ 
+/*
+** Module Init function
+*/
+static int __init hello_world_init(void)
+{
+    pr_info("Welcome to LDD programming\n");
+    pr_info("This is the Simple Hello World Module\n");
+    pr_info("Kernel Module Inserted Successfully...\n");
+    return 0;
+}
+
+/*
+** Module Exit function
+*/
+static void __exit hello_world_exit(void)
+{
+    pr_info("Bye-2 Hello World...\n");
+}
+ 
+module_init(hello_world_init);
+module_exit(hello_world_exit);
+ 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ritesh Mishra>");
+MODULE_DESCRIPTION("A simple hello world driver");
+MODULE_VERSION("2:1.0");
